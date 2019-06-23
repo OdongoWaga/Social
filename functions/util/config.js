5a6b64668fc4a6c045a7a9997a57b4442eb70996
Waga <wagaodongo@gmail.com>
2019-06-23 13:03:19 +0300

    screams

---
+++ functions/util/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    apiKey: "AIzaSyCprHxdhI5DyitCmnp6C1DzVIpXWHXyGVU",
+    authDomain: "screech-1f9d2.firebaseapp.com",
+    databaseURL: "https://screech-1f9d2.firebaseio.com",
+    projectId: "screech-1f9d2",
+    storageBucket: "screech-1f9d2.appspot.com",
+    messagingSenderId: "58635864184",
+    appId: "1:58635864184:web:4f710a1151fe6608"
+  };
